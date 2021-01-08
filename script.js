09af1c5e9e81e9f2e3816a735b6bcaab555b0b15
Rand Hale <randluca@gmail.com>
2021-01-07 19:13:58 -0800

    add new city buttons underneath search form with event listeners

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+$(document).ready(function(){
 
 
-$("#start-search").on("click", function(event){
-    event.preventDefault();
 
-    var newLocation = $("#user-search").eq(0).val();
-    console.log(newLocation);
-})
+    $("#start-search").on("click", function(event){
+        event.preventDefault();
+
+        var newLocation = $("#user-search").eq(0).val();
+        console.log(newLocation);
+
+        var newCityButton = $("<button class=\"city-button\">");
+        newCityButton.text(newLocation);
+        $("#locations").prepend(newCityButton);
+    })
+
+    $(".city-button").on("click", function(){
+        // this function should populate the weather contents but NOT add a new button like in the #start-search event listener.
+    })
+});
